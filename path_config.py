d67df5ae57f27eee9892dd900ac3ee303bd084fc
lx-Meteors <1762204162@qq.com>
2026-01-29 17:39:42 +0800

    Instruct(Full-FT) Init

--- path_config.py
+++ path_config.py
@@ -1,4 +1,4 @@
 
-# this is the Instruct branch
+# this is the Instruct(Full-FT) branch
 import os
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- pretrain/pretrain_script.sh
+++ pretrain/pretrain_script.sh
@@ -1,31 +1,5 @@
+# '../experiment/test_time' is your config path, examples is correct
 
-# python pre_prepare_data.py --work_dir '../experiment/no-pe'
-# python ./pre_trainer.py --work_dir '../experiment/no-pe' --port 14525
-# python ./pre_evaluator.py --work_dir '../experiment/no-pe' --batch_size 1
-
-# python ./pre_trainer.py --work_dir '../experiment/you-pe' --port 14526
-# python ./pre_evaluator.py --work_dir '../experiment/you-pe' --batch_size 1
-
-
-# python pre_prepare_data.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL'
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --port 14574
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL' --batch_size 1
-
-
-
-# python pre_prepare_data.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL'
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL' --port 14574
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_UPL' --batch_size 1
-
-#python pre_prepare_data.py --work_dir '../experiment/local_experiment/ICAE_Llama-3.2-1B_DPL_AEweight-0.75'
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_trainer.py --work_dir '../experiment/main_experiment/ICAE_EPL_1B_MultiChunk' --port 14571
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./pre_evaluator.py --work_dir  '../experiment/main_experiment/ICAE_EPL_1B_MultiChunk'  --batch_size 1
-cd ..
-cd sft
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_trainer.py --work_dir  '../experiment/main_experiment/ICAE_EPL_1B_MultiChunk' --port 14527
-CUDA_VISIBLE_DEVICES=0,1,2,3 python ./instruction_evaluator.py --work_dir  '../experiment/main_experiment/ICAE_EPL_1B_MultiChunk' --batch_size 1
-
-#nohup bash pretrain_script.sh > bash.log 2>&1 &
-# 后台启动方式
-# nohup python ./instruction_trainer.py --work_dir   '../experiment/main_experiment/ICAE_EPL_1B_SingleChunk' --port 14527 > train.log 2>&1 &
-# tail -f train.log
+python pre_prepare_data.py --work_dir '../experiment/test_time'
+python ./pre_trainer.py --work_dir '../experiment/test_time' --port 14572
+python ./pre_evaluator.py --work_dir '../experiment/test_time' --batch_size 1
